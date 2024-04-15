276b3307474121bd4d11265cfa6e50a199eda9df
Ansh Bhardwaj <123256833+AnshBhardwaj001@users.noreply.github.com>
2024-04-15 22:59:10 +0530

    Created Endpoints
    Hotel registration ,  fetch hotel details , placing order and fetch orders are created.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,72 @@
 import express from "express"
 import path from "path"
-import { Server } from "socket.io";
-import http, {get} from "http"
+import {db} from "./config/config.js";
+import {collection, doc, getDoc, getDocs, setDoc} from "firebase/firestore"
 
-
-const app = new express()
-const server = http.createServer(app)
-const io = new Server(server)
+const app = express()
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(process.cwd() , "/public")
 
 app.use(express.static(publicDirectoryPath))
+app.use(express.json())
 
-server.listen(port, () => {
+const hotelsRef = doc(collection(db,'hotels'))
+const orderRef = doc(collection(db,'orders'))
+
+
+//Hotel Endpoints
+app.post('/create' , async (req, res) => {
+    const hotelData = req.body
+
+    await setDoc(hotelsRef, data);  
+
+    res.send("Hotel Added!")
+})
+
+app.get('/hotels', async (req,res) => {
+    const querySnapshot = await getDocs(collection(db, "hotels"));
+    const hotels= [];
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+      hotels.push(doc.data())
+    })
+    res.send(hotels)
+})
+
+
+//Order Endpoints
+app.post('/orders', async (req,res) => {
+    const orderData = req.body
+
+    await setDoc(orderRef , orderData)
+
+    res.send("Order Placed!")
+})
+
+app.get('/order' , async (req,res) => {
+    const querySnapshot = await getDocs(collection(db, "orders"));
+    const orders= [];
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+      orders.push(doc.data())
+    })
+    res.send(orders)
+})
+
+app.get('/allorders' , async (req,res) => {
+    const querySnapshot = await getDocs(collection(db, "orders"));
+    const orders= [];
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+      orders.push(doc.data())
+    })
+    res.send(orders)
+})
+
+app.listen(port, () => {
     console.log('Server is up and running on port: ', port)
 })   
